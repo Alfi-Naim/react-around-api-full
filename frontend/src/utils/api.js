7717f46529ccc2a64b0f9c4e523e0dfa6c70bda3
Alfi Naim <alfons.devs@gmail.com>
2022-09-08 22:36:21 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -90,7 +90,7 @@ class Api {
 const api = new Api({
     // baseUrl: 'http://localhost:3001',
     // baseUrl: 'https://api.alfons.students.nomoreparties.sbs',
-    baseUrl: 'https://api.alfi-usa.students.nomoreparties.sbs',
+    baseUrl: 'https://api.naim-usa.students.nomoredomainssbs.ru',
 });
 
 export default api;
